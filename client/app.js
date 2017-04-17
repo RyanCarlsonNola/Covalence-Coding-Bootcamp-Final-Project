07daf8786a284afc4b38945f3b2bab16d4e1f68f
Ryan Carlson <ryancarlson@Ryans-MacBook-Pro.local>
2017-04-17 17:02:31 -0500

    app.js added

---
+++ client/app.js
@@ -0,0 +1,9 @@
+var app = angular.module('CODEocalypse', []);
+
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+
+    $routeProvider.when('/', {
+        templateUrl: '',
+        controller: ''
+    })
+}])
