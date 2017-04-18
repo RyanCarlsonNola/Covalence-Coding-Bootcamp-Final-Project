823e888fc4a65450098b1e9611bb14f123899f35
James Purcell <jamespurcell@Jamess-MacBook-Pro.local>
2017-04-18 07:07:41 -0500

    added styles

--- client/controllers.js
+++ client/controllers.js
@@ -3,3 +3,19 @@ angular.module('CODEocalypse.controllers', [])
     .controller('WelcomeController', ['$scope', '$location', function($scope, $location) {
         
     }])
+    
+    .controller('JamesWelController', ['$scope', '$location', function($scope, $location) {
+        
+    }])
+
+    .controller('JimWelController', ['$scope', '$location', function($scope, $location) {
+        
+    }])
+
+    .controller('RyanWelController', ['$scope', '$location', function($scope, $location) {
+        
+    }])
+
+    .controller('PatrickWelController', ['$scope', '$location', function($scope, $location) {
+        
+    }])
