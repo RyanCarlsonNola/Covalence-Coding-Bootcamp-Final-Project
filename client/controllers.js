bd445ebfa9d94380600162257766395eaec7c3a4
James Purcell <jamespurcell@Jamess-MacBook-Pro.local>
2017-04-19 16:42:15 -0500

    updated welcome

--- client/controllers.js
+++ client/controllers.js
@@ -2,8 +2,25 @@ angular.module('CODEocalypse.controllers', [])
 
     .controller('WelcomeController', ['$scope', '$location', function($scope, $location) {
         $(document).ready(function(){
-            $('#ja_testdiv').click(function() {
-                $('#ja_testdiv').css('animation', 'none');
+            $('#ja_JamesDiv').mouseover(function() {
+                $(this).css('animation', 'square-to-circle 2s .5s infinite cubic-bezier(1,.015,.295,1.225) alternate');
+            }).mouseout(function(){
+                $(this).css('animation', 'none');
+            });
+            $('#ry_RyanDiv').mouseover(function() {
+                $(this).css('animation', 'square-to-circle 2s .5s infinite cubic-bezier(1,.015,.295,1.225) alternate');
+            }).mouseout(function(){
+                $(this).css('animation', 'none');
+            });
+            $('#ji_JimDiv').mouseover(function() {
+                $(this).css('animation', 'square-to-circle 2s .5s infinite cubic-bezier(1,.015,.295,1.225) alternate');
+            }).mouseout(function(){
+                $(this).css('animation', 'none');
+            });
+            $('#pa_PatrickDiv').mouseover(function() {
+                $(this).css('animation', 'square-to-circle 2s .5s infinite cubic-bezier(1,.015,.295,1.225) alternate');
+            }).mouseout(function(){
+                $(this).css('animation', 'none');
             });
         });
     }])
