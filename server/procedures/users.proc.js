d24a0d841250a41c36f42b07e3db89ea1d9126e2
James Purcell <jamespurcell@Jamess-MacBook-Pro.local>
2017-04-20 10:47:05 -0500

    updated user proc

--- server/procedures/users.proc.js
+++ server/procedures/users.proc.js
@@ -1,1 +1,9 @@
 var db = require("../config/db");
+
+exports.read = function(id) {
+    return db.row('getUser(?)', [id]);
+}
+
+exports.update = function(id, password) {
+    return db.empty("updatePassword(?, ?)", [id, password]);
+}
