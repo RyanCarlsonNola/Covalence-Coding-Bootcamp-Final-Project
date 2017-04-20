45a8973ad07e5cd9026948082604c458dfe978da
Frank James de Lathouder <frankjamesdelathouder@Franks-MacBook-Pro.local>
2017-04-20 08:53:48 -0500

    cookie and pass

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ var bodyParser = require("body-parser");
 var path = require("path");
 var api = require("./api");
 var routeMw = require("./middleware/routing.mw");
+var cookieParser = require("cookie-parser");
+var configurePassport = require("./config/passport");
+
 var app = express();
 
 app.use(express.static(path.join(__dirname, "../client")));
