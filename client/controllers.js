cd78cfad37ec19296479cc95e3cac4c17f36743f
James Purcell <jamespurcell@Jamess-MacBook-Pro.local>
2017-04-25 11:29:07 -0500

    updated the james stuff

--- client/controllers.js
+++ client/controllers.js
@@ -195,10 +195,10 @@ angular.module('CODEocalypse.controllers', [])
         $(document).ready(function () {
             var options = [
                 {selector: '#staggered-test', offset: 50, callback: function(el) {
-                    Materialize.toast("This is our ScrollFire Demo!", 1500 );
+                    
                 } },
                 {selector: '#staggered-test', offset: 205, callback: function(el) {
-                    Materialize.toast("Please continue scrolling!", 1500 );
+                    
                 } },
                 {selector: '#staggered-test', offset: 400, callback: function(el) {
                     Materialize.showStaggeredList($(el));
