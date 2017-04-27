cbce37c151538c3c82905806ee204510ca97333c
James Purcell <jamespurcell@Jamess-MacBook-Pro.local>
2017-04-27 09:16:08 -0500

    removed the port

--- client/factories.js
+++ client/factories.js
@@ -1,7 +1,7 @@
 angular.module('CODEocalypse.factories', [])
 
     .factory('User', ['$resource', function ($resource) {
-        return $resource("http://CODEocalypse.us-east-1.elasticbeanstalk.com:3000/api/users/:id", { id: "@id" }, {
+        return $resource("http://CODEocalypse.us-east-1.elasticbeanstalk.com/api/users/:id", { id: "@id" }, {
             "update": { method: "PUT" }
         })
     }]);
